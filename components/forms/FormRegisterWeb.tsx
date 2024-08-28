1418a2f313a3f43980dfbe51f65a990bd71793aa
panosjapan7 <panosjapan@gmail.com>
2024-08-28 10:26:38 +0200

    2.b User Registration (web)

--- components/forms/FormRegisterWeb.tsx
+++ components/forms/FormRegisterWeb.tsx
@@ -1,7 +1,11 @@
 // ./components/forms/FormRegisterWeb.tsx
-import { MouseEvent, useContext, useEffect, useState } from "react";
+import { MouseEvent, useEffect, useState } from "react";
+import { ActivityIndicator } from "react-native";
 import { router } from "expo-router";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
+import { webAuth } from "../../firebase/firebaseConfig";
+import { StatusType } from "../../types/types";
 import { useGlobalStyles } from "../../styles/stylesheets/globalStyles";
 import { Colors } from "../../styles/colors";
 import "../../styles/css/form.css";
@@ -11,12 +15,10 @@ import {
   validatePassword,
   validateRepeatPassword,
 } from "../../hooks/validations";
-import { AuthContext } from "../../contexts/AuthContext";
 import InputFormWeb from "../inputs/InputFormWeb";
 import ButtonSubmitFormWeb from "../buttons/ButtonSubmitFormWeb";
 
 const FormRegisterWeb = () => {
-  const { setIsLoggedIn } = useContext(AuthContext);
   const { themeHeaderTextColor, themeTextColor } = useGlobalStyles();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -28,11 +30,30 @@ const FormRegisterWeb = () => {
   const [hidePassword, setHidePassword] = useState(true);
   const [hideRepeatPassword, setHideRepeatPassword] = useState(true);
   const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const [status, setStatus] = useState<StatusType>("idle");
 
-  const handleRegister = (e: MouseEvent<HTMLButtonElement>) => {
+  const handleRegister = async (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    setIsLoggedIn(true);
-    router.push("/(drawer)/(tabs)/feed");
+    setStatus("loading");
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        webAuth,
+        email,
+        password
+      );
+      console.log("Success! User registered successfully!");
+      router.replace({
+        pathname: "/login",
+        params: {
+          message: `Please verify your email before logging in. Verification email sent to ${email}`,
+          emailToBeVerified: email,
+        },
+      });
+    } catch (error: any) {
+      console.log("Error ", error.message);
+    } finally {
+      setStatus("idle");
+    }
   };
 
   useDebouncedValidation(
@@ -41,6 +62,7 @@ const FormRegisterWeb = () => {
     setEmailErrorMessage,
     "is invalid"
   );
+
   useDebouncedValidation(
     password,
     validatePassword,
@@ -66,72 +88,78 @@ const FormRegisterWeb = () => {
   }, [email, password, repeatPassword]);
 
   return (
-    <div
-      className="form-container"
-      style={{
-        borderColor: Colors.gray300,
-        borderWidth: 1,
-        borderStyle: "solid",
-      }}
-    >
-      <p
-        className="form-header"
-        style={{
-          color: themeTextColor,
-        }}
-      >
-        Register Screen (web)
-      </p>
-      <InputFormWeb
-        label="Email"
-        value={email}
-        type="email"
-        setValue={setEmail}
-        errorMessage={emailErrorMessage}
-      />
-
-      <InputFormWeb
-        label="Password"
-        value={password}
-        type="password"
-        setValue={setPassword}
-        errorMessage={passwordErrorMessage}
-        hidePassword={hidePassword}
-        setHidePassword={setHidePassword}
-      />
-
-      <InputFormWeb
-        label="Repeat Password"
-        value={repeatPassword}
-        type="password"
-        setValue={setRepeatPassword}
-        errorMessage={repeatPasswordErrorMessage}
-        hidePassword={hideRepeatPassword}
-        setHidePassword={setHideRepeatPassword}
-      />
-
-      <ButtonSubmitFormWeb
-        onClick={handleRegister}
-        isDisabled={isButtonDisabled}
-        caption="Register"
-      />
-
-      <div className="bottomLinksContainer">
-        <a
-          href="/login"
-          onClick={(e) => {
-            e.preventDefault();
-            router.push("/login");
-          }}
+    <>
+      {status === "loading" ? (
+        <ActivityIndicator color={themeHeaderTextColor} size={"large"} />
+      ) : (
+        <div
+          className="form-container"
           style={{
-            color: themeHeaderTextColor,
-            textDecorationColor: themeHeaderTextColor,
+            borderColor: Colors.gray300,
+            borderWidth: 1,
+            borderStyle: "solid",
           }}
         >
-          Have an account? <span>Log in here</span>
-        </a>
-      </div>
-    </div>
+          <p
+            className="form-header"
+            style={{
+              color: themeTextColor,
+            }}
+          >
+            Register Screen (web)
+          </p>
+          <InputFormWeb
+            label="Email"
+            value={email}
+            type="email"
+            setValue={setEmail}
+            errorMessage={emailErrorMessage}
+          />
+
+          <InputFormWeb
+            label="Password"
+            value={password}
+            type="password"
+            setValue={setPassword}
+            errorMessage={passwordErrorMessage}
+            hidePassword={hidePassword}
+            setHidePassword={setHidePassword}
+          />
+
+          <InputFormWeb
+            label="Repeat Password"
+            value={repeatPassword}
+            type="password"
+            setValue={setRepeatPassword}
+            errorMessage={repeatPasswordErrorMessage}
+            hidePassword={hideRepeatPassword}
+            setHidePassword={setHideRepeatPassword}
+          />
+
+          <ButtonSubmitFormWeb
+            onClick={handleRegister}
+            isDisabled={isButtonDisabled}
+            caption="Register"
+          />
+
+          <div className="bottomLinksContainer">
+            <a
+              href="/login"
+              onClick={(e) => {
+                e.preventDefault();
+                router.push("/login");
+              }}
+              style={{
+                color: themeHeaderTextColor,
+                textDecorationColor: themeHeaderTextColor,
+              }}
+            >
+              Have an account? <span>Log in here</span>
+            </a>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 export default FormRegisterWeb;
