340be8bad5e00d1ef8767f2e5fd21c99e09f1b1e
panosjapan7 <panosjapan@gmail.com>
2024-08-31 14:54:27 +0200

    7.a Delete Firebase Account (mobile)

--- app/(drawer)/settings.tsx
+++ app/(drawer)/settings.tsx
@@ -1,21 +1,117 @@
 // ./app/(drawer)/settings.tsx
-import { Text, View } from "react-native";
+import { useContext, useState } from "react";
+import { Alert, Pressable, Text, View } from "react-native";
+import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
 
 import useAuthRedirect from "../../hooks/useAuthRedirect";
+import { AuthContext } from "../../contexts/AuthContext";
 import { useGlobalStyles } from "../../styles/stylesheets/globalStyles";
+import { StatusType } from "../../types/types";
+import FormReauthenticationMobile from "../../components/forms/FormReauthenticationMobile";
+import LoadingIndicator from "../../components/indicators/LoadingIndicator";
+import Spacer from "../../components/utils/Spacer";
 
 const Settings = () => {
   const authRedirect = useAuthRedirect();
+  const { setUser } = useContext(AuthContext);
   const { globalStyles } = useGlobalStyles();
+  const [status, setStatus] = useState<StatusType>("idle");
+  const [showReauthenticationModal, setShowReauthenticationModal] =
+    useState(false);
 
   if (authRedirect) {
     return authRedirect;
   }
 
+  const handleDeleteAccount = async () => {
+    Alert.alert(
+      "Delete Account",
+      "Are you sure you want to delete your account? This action cannot be undone.",
+      [
+        { text: "Cancel", style: "cancel" },
+        {
+          text: "Delete",
+          style: "destructive",
+          onPress: async () => {
+            const currentUser = auth().currentUser;
+
+            if (currentUser) {
+              const isEmailPasswordProvider = currentUser.providerData.some(
+                (provider) => provider.providerId === "password"
+              );
+
+              if (isEmailPasswordProvider) {
+                setShowReauthenticationModal(true);
+              }
+            }
+          },
+        },
+      ]
+    );
+  };
+
+  const reauthenticateWithEmailPassword = async (
+    email: string,
+    password: string
+  ) => {
+    setStatus("loading");
+
+    try {
+      const currentUser = auth().currentUser;
+      if (currentUser && email && password) {
+        const credential = auth.EmailAuthProvider.credential(email, password);
+        await currentUser.reauthenticateWithCredential(credential);
+        deleteUser(currentUser);
+      }
+    } catch (error: any) {
+      Alert.alert("Reauthentication failed", "Please try again.");
+      console.error("Reauthentication failed:", error);
+    } finally {
+      setStatus("idle");
+      setShowReauthenticationModal(false);
+    }
+  };
+
+  const deleteUser = async (currentUser: FirebaseAuthTypes.User) => {
+    setStatus("loading");
+
+    try {
+      await currentUser.delete();
+      setUser(null);
+      Alert.alert(
+        "Account Deleted",
+        "Your account has been deleted successfully."
+      );
+    } catch (error: any) {
+      Alert.alert("Authentication Failed", `Error: ${error}`);
+      console.log("Error", error.message);
+    } finally {
+      setStatus("idle");
+    }
+  };
+
   return (
-    <View style={globalStyles.container}>
-      <Text style={globalStyles.textBlack}>Settings Screen</Text>
-    </View>
+    <>
+      {status === "loading" ? (
+        <LoadingIndicator />
+      ) : (
+        <View style={globalStyles.container}>
+          <Text style={globalStyles.textBlack}>Settings Screen</Text>
+          <Spacer marginBottom={20} />
+          <Pressable onPress={handleDeleteAccount}>
+            <Text style={globalStyles.textMedium}>Delete Account</Text>
+          </Pressable>
+        </View>
+      )}
+
+      <FormReauthenticationMobile
+        visible={showReauthenticationModal}
+        onReauthenticate={reauthenticateWithEmailPassword}
+        onCancel={() => {
+          setShowReauthenticationModal(false);
+        }}
+      />
+    </>
   );
 };
 
---
+++ components/forms/FormReauthenticationMobile.tsx
@@ -0,0 +1,144 @@
+// ./components/forms/FormReauthenticationMobile.tsx
+import { useEffect, useRef, useState } from "react";
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Modal,
+  Platform,
+  Pressable,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+
+import {
+  useDebouncedValidation,
+  validateEmail,
+  validatePassword,
+} from "../../hooks/validations";
+import { useGlobalStyles } from "../../styles/stylesheets/globalStyles";
+import { StatusType } from "../../types/types";
+import LoadingIndicator from "../indicators/LoadingIndicator";
+import InputEmailMobile from "../inputs/InputEmailMobile";
+import InputLabelMobile from "../inputs/InputLabelMobile";
+import InputPasswordMobile from "../inputs/InputPasswordMobile";
+import ButtonSubmitFormMobile from "../buttons/ButtonSubmitFormMobile";
+import Spacer from "../utils/Spacer";
+
+type FormReauthenticationMobileType = {
+  visible: boolean;
+  onReauthenticate: (email: string, password: string) => Promise<void>;
+  onCancel: () => void;
+};
+
+const FormReauthenticationMobile = ({
+  visible,
+  onReauthenticate,
+  onCancel,
+}: FormReauthenticationMobileType) => {
+  const { globalStyles } = useGlobalStyles();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailErrorMessage, setEmailErrorMessage] = useState("");
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState("");
+  const [hidePassword, setHidePassword] = useState(true);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const [status, setStatus] = useState<StatusType>("idle");
+  const passwordInputRef = useRef<TextInput>(null);
+
+  const handleReauthentication = async () => {
+    setStatus("loading");
+    try {
+      await onReauthenticate(email, password);
+    } catch (error: any) {
+      console.log("Error: ", error);
+    } finally {
+      setStatus("idle");
+    }
+  };
+
+  const handleCancel = () => {
+    setEmail("");
+    setPassword("");
+    onCancel();
+  };
+
+  useDebouncedValidation(
+    email,
+    validateEmail,
+    setEmailErrorMessage,
+    "is invalid"
+  );
+
+  useDebouncedValidation(
+    password,
+    validatePassword,
+    setPasswordErrorMessage,
+    "must be at least 6 characters"
+  );
+
+  useEffect(() => {
+    setIsButtonDisabled(!(validateEmail(email) && validatePassword(password)));
+  }, [email, password]);
+
+  return (
+    <Modal visible={visible} transparent={true} animationType="slide">
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+        style={{ flex: 1 }}
+      >
+        {status === "loading" ? (
+          <LoadingIndicator />
+        ) : (
+          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={globalStyles.container}>
+              <Text style={globalStyles.textBold}>
+                Reauthenticate to Delete Account
+              </Text>
+              <Spacer marginVertical={10} />
+              <InputLabelMobile
+                caption="Email "
+                errorMessage={emailErrorMessage}
+              />
+              <InputEmailMobile
+                value={email}
+                setValue={setEmail}
+                returnKeyType="next"
+                goToRef={passwordInputRef}
+              />
+              <InputLabelMobile
+                caption="Password "
+                errorMessage={passwordErrorMessage}
+              />
+              <InputPasswordMobile
+                value={password}
+                setValue={setPassword}
+                ref={passwordInputRef}
+                hidePassword={hidePassword}
+                setHidePassword={setHidePassword}
+                returnKeyType="done"
+              />
+              <Spacer marginVertical={10} />
+              <ButtonSubmitFormMobile
+                onPress={handleReauthentication}
+                buttonText="Confirm Delete"
+                isDisabled={isButtonDisabled}
+              />
+              <Spacer marginVertical={6} />
+
+              <Pressable
+                onPress={handleCancel}
+                style={globalStyles.buttonWithBorder}
+              >
+                <Text style={globalStyles.textMedium}>Cancel</Text>
+              </Pressable>
+            </View>
+          </TouchableWithoutFeedback>
+        )}
+      </KeyboardAvoidingView>
+    </Modal>
+  );
+};
+
+export default FormReauthenticationMobile;
