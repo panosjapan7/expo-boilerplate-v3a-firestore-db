eb8cfc405ebd54e12817530fdadc21b45a658ef5
panosjapan7 <panosjapan@gmail.com>
2024-09-18 15:13:56 +0200

    Removes FieldValue from createdAt, lastLogin type

--- types/databaseTypes.ts
+++ types/databaseTypes.ts
@@ -1,5 +1,5 @@
 // ./types/databaseTypes.ts
-import { Timestamp, FieldValue } from "firebase/firestore";
+import { Timestamp } from "firebase/firestore";
 
 export type AuthProviderType = "password" | "google.com";
 export type UserRole = "Admin" | "Member" | "Viewer";
@@ -12,8 +12,8 @@ export type UserDetailsType = {
   displayName: string | null;
   photoURL: string | null;
   authProviders: AuthProviderType[];
-  createdAt: FieldValue | Timestamp;
-  lastLogin: FieldValue | Timestamp;
+  createdAt: Timestamp;
+  lastLogin: Timestamp;
   role: UserRole[];
   permissions?: PermissionType[]; // Permissions the role of the user has
   magicEmailUsed?: boolean;
