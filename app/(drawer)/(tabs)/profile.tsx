a74e39a26fbf40a45b0b01af73315394254fd227
panosjapan7 <panosjapan@gmail.com>
2024-09-19 13:24:00 +0200

    5. User Profile Dashboard (web, mobile)

--- app/(drawer)/(tabs)/profile.tsx
+++ app/(drawer)/(tabs)/profile.tsx
@@ -1,16 +1,17 @@
 // ./app/(drawer)/(tabs)/profile.tsx
+import { useContext, useEffect } from "react";
 import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 
 import useAuthRedirect from "../../../hooks/useAuthRedirect";
 import { useGlobalStyles } from "../../../styles/stylesheets/globalStyles";
-import { useContext, useEffect } from "react";
 import { AuthContext } from "../../../contexts/AuthContext";
 import { FirebaseFirestoreService } from "../../../services/firestore/FirebaseFirestoreService";
 
 const Profile = () => {
   const authRedirect = useAuthRedirect();
   const { user, userDetails, setUserDetails } = useContext(AuthContext);
-  const { globalStyles } = useGlobalStyles();
+  const { globalStyles, themeBackgroundColor, themeTextColor } =
+    useGlobalStyles();
 
   useEffect(() => {
     const fetchUserDetails = async () => {
@@ -29,97 +30,145 @@ const Profile = () => {
   }
 
   return (
-    <ScrollView contentContainerStyle={globalStyles.container}>
-      {userDetails ? (
-        <>
-          <Image
-            source={{ uri: userDetails.photoURL || undefined }}
-            style={styles.profileImage}
-          />
-
-          <Text style={styles.name}>
-            {userDetails.displayName || "No Name Provided"}
-          </Text>
-          <View style={styles.emailContainer}>
-            <Text style={styles.email}>{userDetails.email}</Text>
-            <Text style={styles.infoText}>
-              {userDetails.emailVerified ? (
-                <Text style={{ color: "green" }}>verified</Text>
-              ) : (
-                <Text style={{ color: "red" }}>unverified</Text>
-              )}
-            </Text>
-          </View>
-
-          <View style={styles.infoSection}>
-            <Text style={styles.infoTitle}>Authentication Providers:</Text>
-            {userDetails.authProviders.map((provider, index) => (
-              <Text key={index} style={styles.infoText}>
-                - {provider}
+    <View style={globalStyles.container}>
+      <ScrollView
+        bounces={true}
+        contentContainerStyle={{ paddingBottom: 20 }}
+        showsVerticalScrollIndicator={false}
+        style={[
+          styles.scrollViewContainer,
+          { backgroundColor: themeBackgroundColor },
+        ]}
+      >
+        {userDetails ? (
+          <>
+            <View style={styles.topSectionContainer}>
+              <Image
+                source={{ uri: userDetails.photoURL || undefined }}
+                style={styles.profileImage}
+                resizeMode="cover"
+              />
+              <Text style={[styles.name, { color: themeTextColor }]}>
+                {userDetails.displayName || "No Name Provided"}
               </Text>
-            ))}
-          </View>
-
-          <View style={styles.infoSection}>
-            <Text style={styles.infoTitle}>Roles:</Text>
-            {userDetails.role.map((role, index) => (
-              <Text key={index} style={styles.infoText}>
-                - {role}
-              </Text>
-            ))}
-          </View>
-
-          {userDetails.permissions && (
-            <View style={styles.infoSection}>
-              <Text style={styles.infoTitle}>Permissions:</Text>
-              {userDetails.permissions.map((permission, index) => (
-                <Text key={index} style={styles.infoText}>
-                  - {permission}
+              <View style={styles.emailContainer}>
+                <Text style={styles.email}>{userDetails.email}</Text>
+                <Text style={styles.infoText}>
+                  {userDetails.emailVerified ? (
+                    <Text style={{ color: "green" }}>verified</Text>
+                  ) : (
+                    <Text style={{ color: "red" }}>unverified</Text>
+                  )}
                 </Text>
-              ))}
+              </View>
             </View>
-          )}
 
-          <View style={styles.infoSection}>
-            <Text style={styles.infoTitle}>Account Details:</Text>
-            <Text style={styles.infoText}>
-              Created At: {userDetails.createdAt.toDate().toDateString()}
-            </Text>
-          </View>
-
-          <View style={styles.infoSection}>
-            <Text style={styles.infoTitle}>Tenant IDs:</Text>
-            {userDetails.tenantIds ? (
-              userDetails.tenantIds.map((tenantId, index) => (
-                <Text key={index} style={styles.infoText}>
-                  - {tenantId}
+            <View style={styles.infoSectionContainer}>
+              <View style={styles.infoSection}>
+                <Text style={[styles.infoTitle, { color: themeTextColor }]}>
+                  Authentication Providers:
                 </Text>
-              ))
-            ) : (
-              <Text style={styles.infoText}>No tenantId</Text>
-            )}
-          </View>
+                {userDetails.authProviders.map((provider, index) => (
+                  <Text
+                    key={index}
+                    style={[styles.infoText, { color: themeTextColor }]}
+                  >
+                    - {provider}
+                  </Text>
+                ))}
+              </View>
 
-          <View style={styles.infoSection}>
-            <Text style={styles.infoTitle}>Primary Tenant ID:</Text>
-            {userDetails.primaryTenantId ? (
-              <Text style={styles.infoText}>{userDetails.primaryTenantId}</Text>
-            ) : (
-              <Text style={styles.infoText}>No primary tenantId</Text>
-            )}
-          </View>
+              <View style={styles.infoSection}>
+                <Text style={[styles.infoTitle, { color: themeTextColor }]}>
+                  Roles:
+                </Text>
+                {userDetails.role.map((role, index) => (
+                  <Text
+                    key={index}
+                    style={[styles.infoText, { color: themeTextColor }]}
+                  >
+                    - {role}
+                  </Text>
+                ))}
+              </View>
 
-          <View style={styles.infoSection}>
-            <Text style={styles.infoTitle}>Magic Email Used:</Text>
-            <Text style={styles.infoText}>
-              {userDetails.magicEmailUsed ? "Yes" : "No"}
-            </Text>
-          </View>
-        </>
-      ) : (
-        <Text>No user details available.</Text>
-      )}
-    </ScrollView>
+              {userDetails.permissions && (
+                <View style={styles.infoSection}>
+                  <Text style={[styles.infoTitle, { color: themeTextColor }]}>
+                    Permissions:
+                  </Text>
+                  {userDetails.permissions.map((permission, index) => (
+                    <Text
+                      key={index}
+                      style={[styles.infoText, { color: themeTextColor }]}
+                    >
+                      - {permission}
+                    </Text>
+                  ))}
+                </View>
+              )}
+
+              <View style={styles.infoSection}>
+                <Text style={[styles.infoTitle, { color: themeTextColor }]}>
+                  Account Details:
+                </Text>
+                <Text style={[styles.infoText, { color: themeTextColor }]}>
+                  Created At: {userDetails.createdAt.toDate().toDateString()}
+                </Text>
+              </View>
+
+              <View style={styles.infoSection}>
+                <Text style={[styles.infoTitle, { color: themeTextColor }]}>
+                  Tenant IDs:
+                </Text>
+                {userDetails.tenantIds ? (
+                  userDetails.tenantIds.map((tenantId, index) => (
+                    <Text
+                      key={index}
+                      style={[styles.infoText, { color: themeTextColor }]}
+                    >
+                      - {tenantId}
+                    </Text>
+                  ))
+                ) : (
+                  <Text style={[styles.infoText, { color: themeTextColor }]}>
+                    No tenantId
+                  </Text>
+                )}
+              </View>
+
+              <View style={styles.infoSection}>
+                <Text style={[styles.infoTitle, { color: themeTextColor }]}>
+                  Primary Tenant ID:
+                </Text>
+                {userDetails.primaryTenantId ? (
+                  <Text style={[styles.infoText, { color: themeTextColor }]}>
+                    {userDetails.primaryTenantId}
+                  </Text>
+                ) : (
+                  <Text style={[styles.infoText, { color: themeTextColor }]}>
+                    No primary tenantId
+                  </Text>
+                )}
+              </View>
+
+              <View style={styles.infoSection}>
+                <Text style={[styles.infoTitle, { color: themeTextColor }]}>
+                  Magic Email Used:
+                </Text>
+                <Text style={[styles.infoText, { color: themeTextColor }]}>
+                  {userDetails.magicEmailUsed ? "Yes" : "No"}
+                </Text>
+              </View>
+            </View>
+          </>
+        ) : (
+          <Text style={[styles.infoTitle, { color: themeTextColor }]}>
+            No user details available.
+          </Text>
+        )}
+      </ScrollView>
+    </View>
   );
 };
 
@@ -131,6 +180,12 @@ const styles = StyleSheet.create({
   center: {
     flex: 1,
     justifyContent: "center",
+    alignItems: "center",
+  },
+  scrollViewContainer: {
+    width: "100%",
+  },
+  topSectionContainer: {
     alignItems: "center",
   },
   profileImage: {
@@ -152,8 +207,11 @@ const styles = StyleSheet.create({
     color: "gray",
     marginBottom: 20,
   },
+  infoSectionContainer: {
+    display: "flex",
+    alignSelf: "center",
+  },
   infoSection: {
-    width: "100%",
     marginVertical: 10,
   },
   infoTitle: {
--- app/(drawer)/(tabs)/profile.web.tsx
+++ app/(drawer)/(tabs)/profile.web.tsx
@@ -1,11 +1,27 @@
 // ./app/(drawer)/(tabs)/profile.web.tsx
+import { useContext, useEffect } from "react";
 import useAuthRedirect from "../../../hooks/useAuthRedirect";
 import "../../../styles/css/profile.css";
 import { useGlobalStyles } from "../../../styles/stylesheets/globalStyles";
+import { AuthContext } from "../../../contexts/AuthContext";
+import { FirebaseFirestoreService } from "../../../services/firestore/FirebaseFirestoreService";
 
 const Profile = () => {
   const authRedirect = useAuthRedirect();
+  const { user, userDetails, setUserDetails } = useContext(AuthContext);
   const { themeTextColor } = useGlobalStyles();
+
+  useEffect(() => {
+    const fetchUserDetails = async () => {
+      if (user) {
+        const details =
+          await FirebaseFirestoreService.getUserDetailsFromFirestore(user.uid);
+        setUserDetails(details);
+      }
+    };
+
+    fetchUserDetails();
+  }, [user, setUserDetails]);
 
   if (authRedirect) {
     return authRedirect;
@@ -14,7 +30,141 @@ const Profile = () => {
   return (
     <div className="profile-wrapper">
       <div className="contents-container">
-        <p style={{ color: themeTextColor }}>Profile Screen (web)</p>
+        {userDetails ? (
+          <div>
+            <div className="profilePicture-section">
+              <img
+                src={userDetails.photoURL || "/assets/defaultProfile.png"}
+                alt="Profile"
+                className="profile-image"
+              />
+              <div className="name-email-container">
+                <h2 className="name" style={{ color: themeTextColor }}>
+                  {userDetails.displayName || "No Name Provided"}
+                </h2>
+
+                <div className="email-container">
+                  <p className="email">{userDetails.email}</p>
+                  <p className="info-text">
+                    {userDetails.emailVerified ? (
+                      <span className="verified">Verified</span>
+                    ) : (
+                      <span className="unverified">Unverified</span>
+                    )}
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="info-section-container">
+              <div className="info-section">
+                <h3 className="info-title" style={{ color: themeTextColor }}>
+                  Authentication Providers:
+                </h3>
+                <ul>
+                  {userDetails.authProviders.map((provider, index) => (
+                    <li
+                      key={index}
+                      className="info-text"
+                      style={{ color: themeTextColor }}
+                    >
+                      {provider}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+
+              <div className="info-section">
+                <h3 className="info-title" style={{ color: themeTextColor }}>
+                  Roles:
+                </h3>
+                <ul>
+                  {userDetails.role.map((role, index) => (
+                    <li
+                      key={index}
+                      className="info-text"
+                      style={{ color: themeTextColor }}
+                    >
+                      {role}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+
+              {userDetails.permissions && (
+                <div className="info-section">
+                  <h3 className="info-title" style={{ color: themeTextColor }}>
+                    Permissions:
+                  </h3>
+                  <ul>
+                    {userDetails.permissions.map((permission, index) => (
+                      <li
+                        key={index}
+                        className="info-text"
+                        style={{ color: themeTextColor }}
+                      >
+                        {permission}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+
+              <div className="info-section">
+                <h3 className="info-title" style={{ color: themeTextColor }}>
+                  Account Details:
+                </h3>
+                <p className="info-text" style={{ color: themeTextColor }}>
+                  Created At:{" "}
+                  {userDetails.createdAt.toDate().toLocaleDateString()}
+                </p>
+              </div>
+
+              <div className="info-section">
+                <h3 className="info-title" style={{ color: themeTextColor }}>
+                  Tenant IDs:
+                </h3>
+                <ul>
+                  {userDetails.tenantIds && userDetails.tenantIds.length > 0 ? (
+                    userDetails.tenantIds.map((tenantId, index) => (
+                      <li
+                        key={index}
+                        className="info-text"
+                        style={{ color: themeTextColor }}
+                      >
+                        {tenantId}
+                      </li>
+                    ))
+                  ) : (
+                    <li className="info-text" style={{ color: themeTextColor }}>
+                      No Tenant IDs
+                    </li>
+                  )}
+                </ul>
+              </div>
+
+              <div className="info-section">
+                <h3 className="info-title" style={{ color: themeTextColor }}>
+                  Primary Tenant ID:
+                </h3>
+                <p className="info-text" style={{ color: themeTextColor }}>
+                  {userDetails.primaryTenantId || "No Primary Tenant ID"}
+                </p>
+              </div>
+
+              <div className="info-section">
+                <h3 className="info-title" style={{ color: themeTextColor }}>
+                  Magic Email Used:
+                </h3>
+                <p className="info-text" style={{ color: themeTextColor }}>
+                  {userDetails.magicEmailUsed ? "Yes" : "No"}
+                </p>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <p>No user details available.</p>
+        )}
       </div>
     </div>
   );
